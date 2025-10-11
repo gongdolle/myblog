3ccc52d93b222b104e011d731cf0d3768024c2a3
gongdolle <tyj456789@naver.com>
2025-10-11 23:07:06 +0900

    database connection def add

---
+++ db/database.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine,Connection
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.pool import QueuePool,NullPool
+from contextlib import contextmanager
+from fastapi import status
+from fastapi.exceptions  import HTTPException
+from dotenv import load_dotenv
+import os,sys
+#debug path
+#sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from env.config import settings
+ 
+DATABASE_URL = f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+
+engine =create_engine(DATABASE_URL,
+                      poolclass=QueuePool,
+                      pool_size=10,max_overflow=0,
+                      pool_recycle=300
+                      )
+
+def direct_get_conn():
+    conn=None
+    try:
+        conn= engine.connect()
+        return conn
+    except SQLAlchemyError as e :
+         print(e)
+         raise e
--- env/config.py
+++ env/config.py
@@ -5,8 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 ENV_PATH = BASE_DIR/".env"
 
-print("현재 작업 경로:", Path.cwd())
-print("읽을 .env 경로:", ENV_PATH)
+
 
 class Settings(BaseSettings):
     DB_USER:str
