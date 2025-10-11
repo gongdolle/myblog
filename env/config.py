c771c00227c3d335d8bc63db30708d8d15d5cbec
gongdolle <tyj456789@naver.com>
2025-10-11 22:12:16 +0900

    path issue fixed

--- env/config.py
+++ env/config.py
@@ -1,6 +1,12 @@
 from pydantic_settings  import BaseSettings
 from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent
+ENV_PATH = BASE_DIR/".env"
+
 print("현재 작업 경로:", Path.cwd())
+print("읽을 .env 경로:", ENV_PATH)
 
 class Settings(BaseSettings):
     DB_USER:str
@@ -12,13 +18,11 @@ class Settings(BaseSettings):
     
 
     model_config = {
-        "env_file": ".env",
+        "env_file": ENV_PATH,
         "env_file_encoding": "utf-8"
     }
 
 
 settings=Settings()
 
-
-print(settings.model_dump())
 
